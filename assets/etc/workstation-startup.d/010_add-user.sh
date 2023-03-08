525040512654402f8eb4d9cba1704d4426f7c4d8
nownabe <nownabe@users.noreply.github.com>
2023-03-08 19:27:45 +0900

    Change default shell to zsh

--- assets/etc/workstation-startup.d/010_add-user.sh
+++ assets/etc/workstation-startup.d/010_add-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-useradd -m "$RUNUSER" -G docker,sudo --shell /bin/bash > /dev/null
+useradd -m "$RUNUSER" -G docker,sudo --shell /bin/zsh > /dev/null
 passwd -d "$RUNUSER" >/dev/null
 echo "%sudo ALL=NOPASSWD: ALL" >> /etc/sudoers
 
