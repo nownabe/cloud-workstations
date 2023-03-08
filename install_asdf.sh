fa9a6d76b591a0039d062def961781fe4cac99aa
nownabe <nownabe@users.noreply.github.com>
2023-03-08 21:53:13 +0900

    Install only plugin for go and python
    Go and python installed by root don't work

--- install_asdf.sh
+++ install_asdf.sh
@@ -12,14 +12,14 @@ declare -A plugins=(
   ["direnv"]="latest"
   ["fzf"]="latest"
   ["ghq"]="latest"
-  ["go-sdk"]="latest"
+  ["go-sdk"]=""
   ["hey"]="latest"
   ["java"]="temurin-17.0.6+10"
   ["jq"]="latest"
   ["nodejs"]="lts"
   ["poetry"]="latest"
   ["protoc"]="latest"
-  ["python"]="latest"
+  ["python"]=""
   ["ripgrep"]="latest"
   ["ruby"]="latest"
   ["terraform"]="latest"
@@ -30,8 +30,12 @@ git clone https://github.com/asdf-vm/asdf.git /root/.asdf --branch "v$ASDF_VERSI
 . /root/.asdf/asdf.sh
 
 for plugin in "${!plugins[@]}"; do
+  echo "** Installing ${plugin} **"
   asdf plugin add "${plugin}"
-  asdf list all "${plugin}" >/dev/null # nodejs lts needs this
-  asdf install "${plugin}" "${plugins[${plugin}]}"
-  asdf global "${plugin}" "${plugins[${plugin}]}"
+
+  if [[ ! -z "${plugins[${plugin}]}" ]]; then
+    asdf list all "${plugin}" >/dev/null # nodejs lts needs this
+    asdf install "${plugin}" "${plugins[${plugin}]}"
+    asdf global "${plugin}" "${plugins[${plugin}]}"
+  fi
 done
